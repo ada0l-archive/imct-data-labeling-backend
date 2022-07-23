04792789b036698fb07b79753cdbf176c99fb47c
ada0l <andreika.varfolomeev@yandex.ru>
2022-07-24 04:53:17 +1000

    created invite dependencies

---
+++ backend/invite/dependencies.py
@@ -0,0 +1,9 @@
+from fastapi import Depends
+from backend.core.database import get_session
+from backend.invite.repository import InviteRepository
+
+
+def get_invite_rep(session=Depends(get_session)):
+    return InviteRepository(session)
+
+
