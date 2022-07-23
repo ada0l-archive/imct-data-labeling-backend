c65383f0d2153a95887a077b46b52000d9845930
ada0l <andreika.varfolomeev@yandex.ru>
2022-07-24 04:53:30 +1000

    create user dependencies

--- backend/user/dependencies.py
+++ backend/user/dependencies.py
@@ -1,5 +1,8 @@
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi_azure_auth.user import User as UserAzureLib
+from backend.core.database import get_session
+from backend.user.models import User
+from backend.user.repository import UserRepository
 
 from backend.user.schemas import (
     azure_scheme,
@@ -7,6 +10,9 @@ from backend.user.schemas import (
     UserAzure
 )
 
+
+def get_user_rep(session=Depends(get_session)):
+    return UserRepository(session)
 
 def get_user_azure(
     user_azure: UserAzureLib = Depends(azure_scheme)
@@ -27,3 +33,16 @@ def get_user_azure_without_error(
         )
     return None
 
+
+async def get_current_user(
+    user_rep: UserRepository = Depends(get_user_rep),
+    azure_user: UserAzure = Depends(get_user_azure)
+) -> User:
+    if azure_user:
+        user = await user_rep.get_by_email(azure_user.email)
+        if user:
+            return user
+    raise HTTPException(
+        detail="The user is not exist",
+        status_code=status.HTTP_403_FORBIDDEN,
+    )
