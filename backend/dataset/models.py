9fb6bf837c0d6097859f276a8b0364b60dbf3f73
ada0l <andreika.varfolomeev@yandex.ru>
2022-07-26 22:05:04 +1000

    project id in dataset table

--- backend/dataset/models.py
+++ backend/dataset/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from backend.core.database import Base
+from backend.project.models import Project
 from backend.user.models import User
 
 
@@ -9,6 +10,10 @@ class Dataset(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
+    project_id = Column(Integer,
+                        ForeignKey(Project.id, ondelete="CASCADE"),
+                        nullable=True)
+    project = relationship("Project")
     creator_id = Column(Integer,
                         ForeignKey(User.id, ondelete="CASCADE"),
                         nullable=True)
