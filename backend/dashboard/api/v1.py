d245197826371e49b56327f54a9e7f70acaedff2
ada0l <andreika.varfolomeev@yandex.ru>
2022-07-29 05:01:21 +1000

    # On branch master # Changes to be committed: #   (use "git restore --staged <file>..." to unstage) # 	modified:   backend/dashboard/api/v1.py

--- backend/dashboard/api/v1.py
+++ backend/dashboard/api/v1.py
@@ -4,6 +4,7 @@ from backend.core.schemas import ListPydantic
 from backend.dashboard.schemas import DashboardPydantic, LabelingProgressPydantic
 from backend.label.dependencies import get_label_rep
 from backend.label.repository import LabelRepository
+from backend.user.dependencies import get_current_user
 
 router = APIRouter()
 
@@ -14,7 +15,8 @@ router = APIRouter()
 )
 async def get_dashboard(
     project_id: int,
-    label_rep: LabelRepository = Depends(get_label_rep)
+    label_rep: LabelRepository = Depends(get_label_rep),
+    _=Depends(get_current_user)
 ):
     labeling_progress = await label_rep.get_labeling_progress(project_id)
     labels = await label_rep.get_labels_with_count(project_id)
