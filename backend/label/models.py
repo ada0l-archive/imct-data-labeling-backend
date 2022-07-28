3d7d10c8c632f537ffd57df41135a93be05d4a9e
ada0l <andreika.varfolomeev@yandex.ru>
2022-07-29 00:05:02 +1000

    label app started

---
+++ backend/label/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from backend.core.database import Base
+from backend.user.models import User
+
+
+class Label(Base):
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    creator_id = Column(Integer,
+                        ForeignKey(User.id, ondelete="CASCADE"),
+                        nullable=True)
+    creator = relationship("User")
